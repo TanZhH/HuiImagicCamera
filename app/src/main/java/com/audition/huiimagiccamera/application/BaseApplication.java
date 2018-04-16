f91ab7e9e2093339be18d4c2acd168b571ee4c08
tanzhuohui <tanzhuohui@orbbec.com>
2018-04-16 19:39:27 +0800

    commit restart

---
+++ app/src/main/java/com/audition/huiimagiccamera/application/BaseApplication.java
@@ -0,0 +1,15 @@
+package com.audition.huiimagiccamera.application;
+
+import android.app.Application;
+
+/**
+ * @author tanzhuohui
+ * @date 2018/3/12
+ */
+
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/audition/huiimagiccamera/present/Present.java
@@ -0,0 +1,9 @@
+package com.audition.huiimagiccamera.present;
+
+/**
+ * @author tanzhuohui
+ * @date 2018/3/12
+ */
+
+public class Present {
+}
