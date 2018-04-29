0182f31ec704a39ce2ee15f6d5764443eff6467d
tanzhuohui <1280136485@qq.com>
2018-04-29 19:30:14 +0800

    Camera

---
+++ app/src/main/java/com/audition/huiimagiccamera/source/HuiCamera.java
@@ -0,0 +1,34 @@
+package com.audition.huiimagiccamera.source;
+
+import android.view.TextureView;
+
+/**
+ * 创建者：   TANHUIHUI
+ * 项  目：   HuiImagicCamera
+ * 包  名：   com.audition.huiimagiccamera.source
+ * 创建日期： 2018/4/29
+ * 描  述：
+ * @author TANHUIHUI
+ */
+
+public class Camera implements SourceInterface{
+    @Override
+    public void init(TextureView textureView) {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void destory() {
+
+    }
+}
