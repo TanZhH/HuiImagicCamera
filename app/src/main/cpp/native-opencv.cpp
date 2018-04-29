164de8a772d0a7faaba49fd364d7b0c011ccb839
tanzhuohui <1280136485@qq.com>
2018-04-29 11:22:32 +0800

    opencv done

---
+++ app/src/main/cpp/native-opencv.cpp
@@ -0,0 +1,4 @@
+//
+// Created by TANHUIHUI on 2018/4/27.
+//
+
---
+++ app/src/main/java/com/audition/huiimagiccamera/natives/OpencvNatives.java
@@ -0,0 +1,12 @@
+package com.audition.huiimagiccamera.natives;
+
+/**
+ * 创建者：   TANHUIHUI
+ * 项  目：   HuiImagicCamera
+ * 包  名：   com.audition.huiimagiccamera.natives
+ * 创建日期： 2018/4/27
+ * 描  述：
+ */
+
+public class OpencvNatives {
+}
